5774c86a0e21438e9003fd95b7c2a1e6debe03f2
Diego <dlbtar@gmail.com>
2025-10-07 11:36:37 -0300

    add decimal value to field value

--- bills/add_bills.php
+++ bills/add_bills.php
@@ -40,7 +40,7 @@ $conn->close();
   <?php include("../navbar/navbar.php")?> 
   <div class="w-xl m-20"> 
     <p><?php echo $message?></p>
-    <form method="POST">
+    <form method="POST">  
         <div class="mb-5 width-50">
           <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Category</label>
           <select id="category" name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required >
@@ -55,7 +55,7 @@ $conn->close();
         </div>
          <div class="mb-5 width-50">
           <label for="bill_value" class="block mb-2 text-sm font-medium text-gray-900">Value</label>
-          <input type="number" id="bill_value" name="bill_value" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="number" step="0.01" id="bill_value" name="bill_value" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
         </div>
          <div class="mb-5 width-50">
           <label for="date_of_payment" class="block mb-2 text-sm font-medium text-gray-900">Date</label>
